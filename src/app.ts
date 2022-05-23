249b138f69910315a60d01fb6410a0fb679bb944
StuBehan <stuart.bainesdinning@gmail.com>
2022-05-24 00:32:32 +0100

    update packages

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-const app = express();
-const port = 8080; // default port to listen
+export const app = express();
+const port = 8080;
 
 app.use(cors());
 
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
 
-// start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
